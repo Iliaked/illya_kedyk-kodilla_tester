aa16cc83d8bca823c29a6c3f43a47496be0589d0
Illya Kedyk <illyakedyk@gmail.com>
2024-01-03 00:05:57 +0100

    Hello World

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Illya");
+        System.out.println(30);
+        System.out.println("K");
+    }
+}
+
+
+
