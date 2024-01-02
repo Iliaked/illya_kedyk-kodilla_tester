357bbec0be85044b4c3ee8e53a4beb7b8e201898
Illya Kedyk <illyakedyk@gmail.com>
2024-01-03 00:41:07 +0100

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String city = "Kraków";
+        String street = "Stolarska";
+        int streetNumber = 1;
+        double pi = 3.14;
+        boolean logic = (true);
+        char letter = 'Q';
+        System.out.println(city);
+        System.out.println(street);
+        System.out.println(streetNumber);
+        System.out.println(pi);
+        System.out.println(logic);
+        System.out.println(letter);
+    }
+}
